d37c83dea47953b4e15cbe4617f5a0dbf86b858f
Vivi271 <142178270+Vivi271@users.noreply.github.com>
2025-04-02 22:39:50 -0500

    Revert "subir registros a la base de datos"
    This reverts commit 92853678f618b7d566fa7f34913008cfc8231473.

--- pqrs_app/forms.py
+++ pqrs_app/forms.py
@@ -2,9 +2,6 @@ from django import forms
 from .models import Cliente
 
 class ClienteForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
     class Meta:
         model = Cliente
         fields = [
@@ -15,7 +12,6 @@ class ClienteForm(forms.ModelForm):
             'telefono_movil',
             'tipo',
             'validacion',
-            'password',
         ]
         widgets = {
             'tipo_identificacion': forms.Select(attrs={'class': 'form-control'}),
@@ -26,12 +22,3 @@ class ClienteForm(forms.ModelForm):
             'tipo': forms.Select(attrs={'class': 'form-control'}),
             'validacion': forms.Select(attrs={'class': 'form-control'}),
         }
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-
-        if password != confirm_password:
-            raise forms.ValidationError("Las contraseñas no coinciden.")
-        return cleaned_data
