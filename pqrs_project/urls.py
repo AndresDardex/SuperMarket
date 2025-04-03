b1ae293bb16dcfb849aa9ff8e6d10925d8f59f5c
Vivi271 <142178270+Vivi271@users.noreply.github.com>
2025-04-02 22:28:57 -0500

    Actualizacion Home

--- pqrs_project/urls.py
+++ pqrs_project/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from pqrs_app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('registrar/', views.registrar_usuario, name='registrar_usuario'),  # Nueva ruta
+    path('admin/login/', auth_views.LoginView.as_view(template_name='admin/login.html')),
 ]
+
+#Validar Validar *2
+
+#check
